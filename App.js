8b0c403a605b04d51f97ec56f1f1fbcc4c714e09
huangmickey <67403573+huangmickey@users.noreply.github.com>
2022-03-07 20:00:56 -0800

    Fixed text in App.JS to original

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello WillCode4Food!ASDASDASDASDASDASDASasdsadDASdASDAASDASD</Text>
+      <Text>Hello WillCode4Food!</Text>
       <StatusBar style="auto" />
     </View>
   );
