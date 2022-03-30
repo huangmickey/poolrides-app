2fa40d00b280bb18226ac07322c9afa0f8948588
EnriqueAlv1332 <44791102+EnriqueAlv1332@users.noreply.github.com>
2022-03-30 12:15:36 -0700

    added logo to startup and made sign up clickable

--- src/feature-or-screens/Startup.js
+++ src/feature-or-screens/Startup.js
@@ -1,70 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
-import { StyleSheet, Text, View, Image } from "react-native";
-import { TouchableOpacity } from "react-native-web";
-import { colors } from "../utils/colors";
-import { textDecorationLine } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
-
-export default function Startup() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image
-          style={styles.logo}
-          source={require("../../assets/splash.png")}
-        />
-        <Text style={styles.text}>PoolRides</Text>
-      </View>
-      <View style={styles.signUp}>
-          <TouchableOpacity
-            onPress = {() => naviagation.navigate()}
-            >
-                <Text style ={styles.signUpStyle}>New to PoolRides? Sign Up.</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "black",
-      alignItems: "center",
-    },
-    logo: {
-      width: 153,
-      height: 58,
-      resizeMode: "contain",
-      position: "absolute",
-    },
-    text: {
-      color: "#abb5be",
-      position: "relative",
-      top: 60,
-      fontSize: 16,
-      fontWeight: "600",
-    },
-    logoContainer: {
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "flex-start",
-      marginTop: 140,
-    },
-    signUpStyle:{
-        color: "white",
-        position: "absolute",
-        textDecorationLine: "underline", 
-        top: 600,
-        left: -90
-    }
-
-
-  });
-=======
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image, Button } from "react-native";
 import CustomButton from "../components/CustomButton";
 import { colors } from "../utils/colors";
+
+
 
 
 export default function Startup({ navigation }) {
@@ -83,7 +22,9 @@ export default function Startup({ navigation }) {
     return (
         <View style={styles.screenContainer}>
             <View style={styles.logoContainer}>
-                <Text style={styles.text}>Pool Rides Logo</Text>
+            <Image
+              style={styles.logo} source={require("../../assets/splash.png")}
+            />
             </View>
             <View style={styles.sectionContainer}>
                 <View style={styles.buttonBox}>
@@ -91,7 +32,11 @@ export default function Startup({ navigation }) {
                 </View>
                 <View style={styles.buttonBox2}>
                     <CustomButton title={"Rider Login"} color={colors.mint} textColor={colors.black} onPress={riderLoginPressHandler}/>
-                    <Text style={styles.textSignUp}>Sign up text button</Text>
+                    
+                </View>
+                <View>
+          
+                  <Button title="New to PoolRides? Sign Up." color={'white'} onPress={signUpPressHandler}/>
                 </View>
                 <View style={styles.buttonBox}>
                     
@@ -140,9 +85,16 @@ const styles = StyleSheet.create({
     text: {
         color: "white",
     },
+    logo: {
+      width: 153,
+      height: 58,
+      resizeMode: "contain",
+      position: "absolute",
+    },
     textSignUp: {
         color: "white",
+        textDecorationLine: "underline", 
         marginTop: "5%"
     }
 });
->>>>>>> origin/main
+
