6ed7db04a0d62a021666c3a1d60b5b23bbce1480
EnriqueAlv1332 <44791102+EnriqueAlv1332@users.noreply.github.com>
2022-04-29 15:24:00 -0700

    PR-5 database referencing help

--- src/screens/dashboard/RiderDashboard.js
+++ src/screens/dashboard/RiderDashboard.js
@@ -4,6 +4,9 @@ import DashboardButton from '../../components/DashboardButton';
 import { AppStyles } from '../../utils/styles';
 import { db, authentication } from '../../firebase/firebase-config';
 import { BottomNavigation } from 'react-native-paper';
+import { getAuth } from "firebase/auth";
+import {doc, getDoc} from 'firebase/firestore/lite';
+
 import CustomButton from '../../components/CustomButton';
 
 
@@ -13,6 +16,37 @@ export default function RiderDashboard({ navigation }) {
     authentication.signOut();
   }
 
+//const auth = getAuth();  
+//const email = auth.currentUser.email;
+
+
+async function checkDB(){
+  
+  const userUID = authentication.currentUser.uid;
+
+  const userDocReference = doc(db,"users",userUID);
+  const userDocSnapshot = await getDoc(userDocReference);
+
+  
+  if(userDocSnapshot.exists()){
+
+    const userFirstName = userDocSnapshot.data().firstname;
+    console.log(userFirstName);
+  }
+  else{
+    console.log("No Such Document!");
+  }
+}
+
+//const fName = checkDB();
+
+  function testing(){
+    
+      console.log(auth.currentUser.email);
+    
+  }
+
+  //want to add some icons to buttons
   function PressHandlerDashboard(title){
     switch(title){
       case 'A':
@@ -26,9 +60,13 @@ export default function RiderDashboard({ navigation }) {
       case 'C':
         console.log("c");
         //navigation.navigate()
+        //testing for output
+        checkDB();
         break;
       case 'D':
         console.log("d");
+        testing();
+        
         //navigation.navigate()
         break;
       default:
@@ -67,12 +105,13 @@ export default function RiderDashboard({ navigation }) {
       <Item title={item.title} />
     );
   
+
+
     return (
-      
       <SafeAreaView style={styles.container}>
-        <Text style = {styles.text}>Welcome Driver! This is Driver Dashboard</Text>
-        <Text style = {styles.text}>Maybe we can make this your profile or friend activity?
-        make profile button an edit profile button? Or we can put your current location on this screen?</Text>
+        <Text style = {styles.text}>{}</Text>
+        <Text style = {styles.text}>Name should be in Here^^ press C and it'll go to console.This is Rider Dashboard</Text>
+        <Text style = {styles.text}>I think putting something here could be good, Kurtis made his look more similar to the mockups if not</Text>
         <CustomButton 
             title='Log out' 
             color={AppStyles.color.mint} 
@@ -111,9 +150,9 @@ const styles = StyleSheet.create({
     item: {
       justifyContent: 'space-between',
       alignSelf: 'flex-end',
-      padding: 8,
+      padding: 2,
       marginVertical: 8,
-      marginHorizontal: 5,
+      marginHorizontal: 0,
     },
     title: {
       fontSize: 32,
